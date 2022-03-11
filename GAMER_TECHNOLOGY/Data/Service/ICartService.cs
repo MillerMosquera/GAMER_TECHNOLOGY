315763013a87458284e56f034da8e02c0c52e046
Miller Palacios <44532720+MillerPalacios@users.noreply.github.com>
2022-03-11 16:02:34 -0500

    LocalStorage y funciones del carrito

---
+++ GAMER_TECHNOLOGY/Data/Service/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using GAMER_TECHNOLOGY.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GAMER_TECHNOLOGY.Data.Service
+{
+    public interface ICartService
+    {
+        event Action OnChange;
+
+        Task AddToCart(CartItem item);
+        Task DeleteItem(CartItem item);
+        Task EmptyCart();
+        Task<List<CartItem>> GetCartItems();
+    }
+}
